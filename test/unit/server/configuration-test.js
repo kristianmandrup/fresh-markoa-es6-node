f031b7647b7e7c6d8b037fc63c77303c9ce662de
Kristian Mandrup <kmandrup@gmail.com>
2015-07-21 00:55:19 +0100

    configuration test skel

--- test/unit/server/configuration-test.js
+++ test/unit/server/configuration-test.js
@@ -1,7 +1,26 @@
 import Configuration from '../../../src/server/configuration';
 
-describe('Configuration', function() {
+describe('Configuration', () => {
   it('exists', () => {
     expect(Configuration).to.not.be.undefined;
+  });
+
+  describe('#settings', () => {
+  });
+
+  describe('#rootPath', () => {
+  });
+
+  describe('#utils', () => {
+  });
+
+  describe('#current', () => {
+    // check rootPath
+  });
+
+  describe('#default(name)', () => {
+  });
+
+  describe('#middleware(name)', () => {
   });
 });
