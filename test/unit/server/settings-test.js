3eb41a87646da6ab5a99232cae5f41fd83a239a0
Kristian Mandrup <kmandrup@gmail.com>
2015-07-20 18:17:16 +0100

    Configuration and settings test

--- test/unit/server/settings-test.js
+++ test/unit/server/settings-test.js
@@ -1,17 +1,42 @@
-'use strict';
-
 import Settings from '../../../src/server/settings';
+import path from 'path';
 
-describe('Settings', function() {
+describe('Settings', () => {
   it('exists', () => {
     expect(Settings).to.not.be.undefined;
   });
 
-  describe('empty config', function() {
-    var defaults = new Settings({});
+  describe('empty config', () => {
+    let settings = new Settings({});
 
     it('has config', () => {
-      expect(defaults.config).to.eql({});
+      expect(settings.config).to.eql({});
+    });
+
+    it('#rootPath', () => {
+      let myRoot = path.resolve(path.join(__dirname, '../../../'));
+      expect(settings.rootPath).to.eql(myRoot);
+    });
+  });
+
+  describe('#config', () => {
+    let myRoot = 'path/to/root';
+    let config = {};
+    let options = {rootPath: myRoot};
+    let settings = new Settings(config, options);
+
+    describe('#rootPath', () => {
+      expect(settings.rootPath).to.eql(myRoot);
+    });
+
+    describe('#myRoot', () => {
+      it('not same as rootPath', () => {
+        expect(settings.myRoot).to.not.eql(myRoot);
+      });
+
+      it('not same as rootPath', () => {
+        expect(settings.myRoot).to.not.eql('');
+      });
     });
   });
 });
