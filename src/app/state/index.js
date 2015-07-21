fb557aab4d5cf84333432f9fccc04991846257f7
Kristian Mandrup <kmandrup@gmail.com>
2015-07-21 01:08:11 +0100

    mount data on state

--- src/app/state/index.js
+++ src/app/state/index.js
@@ -1,8 +1,20 @@
 import Configurator from '../../server/configurator';
-// import Data from './data';
+import Data from './data';
 
 export default class State extends Configurator {
-  constructor(config) {
+  constructor(config, data = {}) {
     super(config);
+    this.data = new Data(config. data)
+  }
+
+  merge(data) {
+    Object.assign(this.data, data);
+  }
+
+  mount(data, name) {
+    if (typeof name !== 'string') {
+      throw `Must mount data Object on named key, was: ${name}`;
+    }
+    Object.assign(this.data[name], data)
   }
 }
